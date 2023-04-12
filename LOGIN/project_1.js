969d61515438d40e86e16eefeb7fc0ae3e496cec
HarvsDucs <124319998+HarvsDucs@users.noreply.github.com>
2023-04-12 21:21:43 +0800

    Add files via upload

---
+++ LOGIN/project_1.js
@@ -0,0 +1,8 @@
+
+function gf() {
+  document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+function try1() {
+  document.getElementById("gg").innerHTML = "Paragraph has changed.";
+}
